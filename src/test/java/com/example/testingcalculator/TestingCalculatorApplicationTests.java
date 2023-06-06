96a0134242ffedeb626aef098dde4a4b5436602e
Admin <anstdor@gmail.com>
2023-06-06 11:11:53 +0300

    Initial commit

---
+++ src/test/java/com/example/testingcalculator/TestingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
